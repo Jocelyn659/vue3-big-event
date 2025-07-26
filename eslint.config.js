d0b2f3af322e01a28102a92188a9daf91f79e672
jiayao meng <jiayaomeng@jiayaos-MacBook-Pro.local>
2025-07-26 23:27:16 +0800

    错误演示

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}']
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+  // 自定义 ESLint 规则
+  {
+    plugins: {
+      // 确保值是导入的插件对象（prettierPlugin）
+      prettier: prettierPlugin
+    },
+    rules: {
+      //前置：
+      //禁用VSCode 的Prettier 插件
+      //安装Eslint插件后并在settings.json配置保存自动修复
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true, // 单引号
+          semi: false, // 无分号
+          printWidth: 80, // 每行宽度至多80字符
+          trailingComma: 'none', // 不加对象|数组最后逗号
+          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+        }
+      ],
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      'no-undef': 'off' // 未定义变量错误提示
+    }
+  }
+])
